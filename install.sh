279bf7a794e54914eb0b934d3fd00d3b5a65e2e6
Tor Inge Skaar <tor.inge.skaar@gmail.com>
2014-06-17 14:06:40 +0200

    Added some comments

--- install.sh
+++ install.sh
@@ -7,16 +7,22 @@ if [ "$UID" -ne 0 ]; then
     exit
 fi
 
+# Install CC2ASN configuration file
 cp cc2asn.conf /etc/default/cc2asn
 
+# Install Upstart configuration
 cp cc2asn.upstart /etc/init/cc2asn.conf
+initctl reload-configuration
 
+# Install downloader and update config path
 cp RIR-downloader.sh /usr/local/sbin/
 sed -i 's/cc2asn.conf/\/etc\/default\/cc2asn/' /usr/local/sbin/RIR-downloader.sh 
 
+# Install parser and update config path
 cp SEF-parser.py /usr/local/sbin/
 sed -i 's/cc2asn.conf/\/etc\/default\/cc2asn/' /usr/local/sbin/SEF-parser.py
 
+# Install server
 cp cc2asn-server.py /usr/local/sbin/
 
 echo "Installation of CC2ASN is complete"
